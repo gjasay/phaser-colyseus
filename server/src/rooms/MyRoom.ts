499c1e8106a13155af1eab3e19d757bd9e70ba73
gjasay <gabeasay@gmail.com>
2025-01-18 14:24:41 -0500

    send playerId message

--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -2,7 +2,7 @@ import { Room, Client } from "@colyseus/core";
 import { PongRoomState } from "./schema/PongRoomState";
 
 export class MyRoom extends Room<PongRoomState> {
-  maxClients = 4;
+  public maxClients = 2;
 
   onCreate (options: any) {
     this.setState(new PongRoomState());
@@ -10,11 +10,13 @@ export class MyRoom extends Room<PongRoomState> {
 
   }
 
-  onJoin (client: Client, options: any) {
+  onJoin (client: Client, _options: any) {
     console.log(client.sessionId, "joined!");
+    client.send("playerId", this.clients.length);
+    console.log("playerId set:", this.clients.length);
   }
 
-  onLeave (client: Client, consented: boolean) {
+  onLeave (client: Client, _consented: boolean) {
     console.log(client.sessionId, "left!");
   }
 
